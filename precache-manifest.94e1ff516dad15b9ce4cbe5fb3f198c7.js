e4dbd66f3110c97e2b8843843cadcb9819a227f1
Nandiheath <nandi0315@gmail.com>
2019-08-05 09:48:54 +0800

    Updates

---
+++ precache-manifest.94e1ff516dad15b9ce4cbe5fb3f198c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32a2c9fb4360314b4ba9",
+    "url": "/district-councils-dashboard/static/js/runtime~main.8cf94a55.js"
+  },
+  {
+    "revision": "b0a2b7e8a800dc1212e8",
+    "url": "/district-councils-dashboard/static/js/main.d53de676.chunk.js"
+  },
+  {
+    "revision": "0aee8fd4bb006694946d",
+    "url": "/district-councils-dashboard/static/js/2.4d21e9ad.chunk.js"
+  },
+  {
+    "revision": "b0a2b7e8a800dc1212e8",
+    "url": "/district-councils-dashboard/static/css/main.c7c565a7.chunk.css"
+  },
+  {
+    "revision": "3411e7cd4aa9b5656b138d4388088f9d",
+    "url": "/district-councils-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/district-councils-dashboard/precache-manifest.4c6ab2156c6012f176452c2cdb87e17f.js"
+  "/district-councils-dashboard/precache-manifest.94e1ff516dad15b9ce4cbe5fb3f198c7.js"
 );
 
 workbox.clientsClaim();
